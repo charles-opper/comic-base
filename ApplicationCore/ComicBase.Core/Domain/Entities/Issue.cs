aa3bd115b04cd5e4bc92ed2cd6fdbe5b4878bbad
Charles Opper <44379441+charles-opper@users.noreply.github.com>
2018-12-22 15:27:55 -0500

    Project Skeletons Complete

---
+++ ApplicationCore/ComicBase.Core/Domain/Entities/Issue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ComicBase.Core.Domain.Entities
+{
+    public class Issue
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        public int Volume { get; set; }
+
+        [Required]
+        public int IssueNumber { get; set; }
+
+        public virtual Publisher Publisher { get; set; }
+    }
+}
---
+++ ApplicationCore/ComicBase.Core/Domain/Entities/Publisher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ComicBase.Core.Domain.Entities
+{
+    public class Publisher
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string SiteUrl { get; set; }
+    }
+}
